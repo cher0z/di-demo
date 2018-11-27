45a7b3015b7234ff6487fd836e1af24a10810b58
cher0z <reni.rasa@gmail.com>
2018-11-27 20:10:55 +0100

    Trying out git

--- src/main/java/com/renaldorasa/springcourse/didemo/Controllers/MyController.java
+++ src/main/java/com/renaldorasa/springcourse/didemo/Controllers/MyController.java
@@ -1,4 +1,13 @@
 package com.renaldorasa.springcourse.didemo.Controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
+
+    public String hello(){
+        System.out.println("Hello!!!");
+
+        return "whatever";
+    }
 }
--- src/main/java/com/renaldorasa/springcourse/didemo/DiDemoApplication.java
+++ src/main/java/com/renaldorasa/springcourse/didemo/DiDemoApplication.java
@@ -1,12 +1,17 @@
 package com.renaldorasa.springcourse.didemo;
 
+import com.renaldorasa.springcourse.didemo.Controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController controller = (MyController) ctx.getBean("myController");
+        controller.hello();
     }
 }
