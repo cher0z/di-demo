a7902b534ee5e6422323b5d6c6ad034dad794474
cher0z <reni.rasa@gmail.com>
2018-11-29 01:44:53 +0100

    @primary, profiles, default profiles

--- src/main/java/com/renaldorasa/springcourse/didemo/controllers/MyController.java
+++ src/main/java/com/renaldorasa/springcourse/didemo/controllers/MyController.java
@@ -1,13 +1,20 @@
 package com.renaldorasa.springcourse.didemo.controllers;
 
+import com.renaldorasa.springcourse.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello(){
         System.out.println("Hello!!!");
 
-        return "whatever";
+        return greetingService.sayGreeting();
     }
 }
