04f0b12653737b558380da751671bf1e114f3068
cher0z <reni.rasa@gmail.com>
2018-11-30 20:18:24 +0100

    some more changes

--- src/main/java/com/renaldorasa/springcourse/didemo/services/PrimarySpanishGreetingService.java
+++ src/main/java/com/renaldorasa/springcourse/didemo/services/PrimarySpanishGreetingService.java
@@ -7,9 +7,16 @@ import org.springframework.stereotype.Service;
 @Service
 @Profile("es")
 @Primary
-public class PrimarySpanishGreetingService implements GreetingService{
+public class PrimarySpanishGreetingService implements GreetingService {
+
+    private GreetingRepository greetingRepository;
+
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "Servicio de saludo primario";
+        return greetingRepository.getSpanishGreeting();
     }
 }
